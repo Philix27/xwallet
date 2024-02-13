bf810c6421042032ccc5f4aa5981fe88401a92a3
Philix27 <philixbob@gmail.com>
2024-02-13 08:01:54 +0100

    chore: sendMsgToPhone added to Notification service

--- src/integration/termii/mod.rs
+++ src/integration/termii/mod.rs
@@ -1,2 +1,35 @@
 pub mod consts;
-mod sendMsg;
+
+use std::fmt::Error;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PhoneBody {
+    pub api_key: String,
+    pub to: String,
+    pub from: String,
+    pub sms: String,
+    /// type = plain
+    #[serde(rename = "type")]
+    pub types: String,
+    pub channel: Channel,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Channel {
+    #[serde(rename = "generic")]
+    Generic,
+    #[serde(rename = "whatsapp")]
+    Whatsapp,
+    #[serde(rename = "dnd")]
+    Dnd,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PhoneResponse {
+    pub message_id: String,
+    pub message: String,
+    pub balance: u64,
+    pub user: String,
+}
